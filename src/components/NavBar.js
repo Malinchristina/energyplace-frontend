e54c9528d1c9951bb252de967dbe386f2250779c
Malinchristina <malin_christina@yahoo.se>
2024-11-02 10:15:29 +0100

    feat(auth): update to reflect current logged-in user's state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/logo.webp';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i class="fa-solid fa-right-to-bracket"></i>Sign in
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i class="fa-solid fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
       <Container>
@@ -24,20 +46,7 @@ const NavBar = () => {
               >
                 <i class="fa-solid fa-house"></i>Home
               </NavLink>
-              <NavLink
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-                to="/signin"
-              >
-                <i class="fa-solid fa-right-to-bracket"></i>Sign in
-              </NavLink>
-              <NavLink
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-                to="/signup"
-              >
-                <i class="fa-solid fa-user-plus"></i>Sign up
-              </NavLink>
+              {currentUser ? loggedInIcons : loggedOutIcons}
             </Nav>
         </Navbar.Collapse>
       </Container>
@@ -45,4 +54,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar
+export default NavBar;
