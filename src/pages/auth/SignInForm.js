0a7865057bfa72e3c58435ceca105c4d812a8f26
Malinchristina <malin_christina@yahoo.se>
2024-11-10 16:12:41 +0100

    feat(hooks): implement useRedirect hook to manage user navigation based on authentication status

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,9 +16,11 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import bg from "../../assets/bg.webp"
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
   
   const [signInData, setSignInData] = useState({
     username: "",
@@ -35,7 +37,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.push("/");
+      history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
     }
